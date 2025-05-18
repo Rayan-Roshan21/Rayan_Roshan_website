d6dc47b64f71116ef5d18c580e1cc4a356624569
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-17 23:53:28 -0400

    Added text and copyright for the about page.

--- Rayan_Roshan_Website/src/Pages/About.jsx
+++ Rayan_Roshan_Website/src/Pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import '../Pages_CSS/About.css';
 import { motion } from 'framer-motion';
+import Copyright from '../Copyright_title/Copyright_title'
 
 function About() {
   return (
@@ -14,6 +15,16 @@ function About() {
       <Sidebar />
       <h1 className="name">Rayan Roshan</h1>
       <h2 className='about-title'>About</h2>
+      <div>
+        <h2 className='Subtitle'>Who am I</h2>
+         <p class = "paragraph">I'm a computer science student passionate about building innovative software and solving real-world problems through technology. I have experience in mobile app development, with a focus on Swift and Firebase, and enjoy exploring new technologies in the fields of AI and data science. As an entrepreneur, I love turning ideas into scalable products and am always looking for new challenges to learn and grow.</p>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        <Copyright />
+      </div>
     </motion.div>
   );
 }
