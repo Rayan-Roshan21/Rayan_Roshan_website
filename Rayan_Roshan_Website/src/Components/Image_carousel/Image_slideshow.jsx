3f0797b6e0b1d64e685a2eaa9c23d55fec911ab2
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-20 18:45:42 -0400

    Made adjustments with the image carousel and other elements for the about page.

--- Rayan_Roshan_Website/src/Components/Image_carousel/Image_slideshow.jsx
+++ Rayan_Roshan_Website/src/Components/Image_carousel/Image_slideshow.jsx
@@ -1,6 +1,8 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../Image_carousel/Image_slideshow.css"
+import Slider from "react-slick";
+
 const images = [
   "https://res.cloudinary.com/demo/image/upload/v1652345767/docs/demo_image2.jpg",
   "https://res.cloudinary.com/demo/image/upload/v1652366604/docs/demo_image5.jpg",
@@ -9,13 +11,20 @@ const images = [
 function Image_slideshow() {
   return (
     <div className="box">
-      <Carousel useKeyboardArrows={true}>
+      <Carousel 
+        useKeyboardArrows={true} 
+        autoPlay={true}         // Enables auto-play
+        interval={3000}         // 3000ms = 3 seconds between slides
+        infiniteLoop={true}     // Loop back to the start
+        showThumbs={false}      // Optional: hides image thumbnails
+        stopOnHover={true}      // Optional: pause auto-play on hover
+        >
         {images.map((URL, index) => (
-          <div className="slide">
-            <img alt="sample_file" src={URL} key={index} />
-          </div>
+            <div className="slide" key={index}>
+            <img alt="sample_file" src={URL} />
+            </div>
         ))}
-      </Carousel>
+    </Carousel>
     </div>
   );
 }
