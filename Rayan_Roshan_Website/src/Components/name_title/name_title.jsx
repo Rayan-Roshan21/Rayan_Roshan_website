ccfdfe24de20e3686c1869891df4c451d9af88d2
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-19 23:07:52 -0400

    Made adjustments with the title

--- Rayan_Roshan_Website/src/Components/name_title/name_title.jsx
+++ Rayan_Roshan_Website/src/Components/name_title/name_title.jsx
@@ -1,7 +1,12 @@
 import '../Name_title/Name_title.css'
+import { Link } from 'react-router-dom';
 function Name_title() {
     return (
-        <h1 className="title">Rayan Roshan</h1>
+
+        <Link to="/" className="title">
+            <h1 className="title">Rayan Roshan</h1>
+        </Link>
+
     );
 }
 export default Name_title;
