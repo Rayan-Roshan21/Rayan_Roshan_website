04a888a8666676a6419443de92ee086fc89f3829
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-19 23:43:30 -0400

    Made changes to the css file.

--- Rayan_Roshan_Website/src/Components/name_title/name_title.jsx
+++ Rayan_Roshan_Website/src/Components/name_title/name_title.jsx
@@ -1,9 +1,10 @@
 import '../Name_title/Name_title.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 function Name_title() {
+    const location = useLocation();
+    const isHome = location.pathname === '/';
     return (
-
-        <Link to="/" className="title">
+        <Link to="/" className={`title ${!isHome ? 'title-alt' : ''}`}>
             <h1 className="title">Rayan Roshan</h1>
         </Link>
 
