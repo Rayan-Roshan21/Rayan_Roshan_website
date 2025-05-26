bce20a800c7368cf5f89b32dfac1651cbb8df0e7
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-26 00:47:09 -0400

    Made adjustments with the title.

--- Rayan_Roshan_Website/src/Pages/Projects.jsx
+++ Rayan_Roshan_Website/src/Pages/Projects.jsx
@@ -1,4 +1,17 @@
+import React from 'react';
+import Name_title from '../Components/Name_title/Name_title';
+import Sidebar from '../Components/Sidebar/Sidebar.jsx';
+import Copyright_title from '../Components/Copyright_title/Copyright_title.jsx';
+import '../Pages_CSS/Projects.css';
 function Projects() {
-  return <h1>Contact Page</h1>;
+  return (
+    <>
+    <div>
+      <Sidebar />
+      <Name_title />
+    </div>
+    <h2 className='Projects-title'>Projects</h2>
+    </>
+  );
 }
 export default Projects;
