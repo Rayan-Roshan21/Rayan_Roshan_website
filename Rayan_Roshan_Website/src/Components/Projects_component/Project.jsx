5077940e3d8c3ec93440e5a88edf2c64032e92bf
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-26 21:11:43 -0400

    Added some css to a project.

--- Rayan_Roshan_Website/src/Components/Projects_component/Project.jsx
+++ Rayan_Roshan_Website/src/Components/Projects_component/Project.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import '../Projects_component/Project.css'; // Assuming you have a CSS file for styling
 function Project(props) {
     return (
+        <>
         <div className='project-container'>
-            <p>{props.name}</p>
-            <p>{props.description}</p>
+            <p className='project-image'>{props.image}</p>
+            <p className='project_name'>{props.name}</p>
+            <p className='project-description'>{props.description}</p>
             <p>{props.technologies}</p>
             <p>{props.link}</p>
-            <p>{props.image}</p>
             <p>{props.github}</p>
             <p>{props.year}</p>
             <p>{props.status}</p>
         </div>
+       </>
     );
 }
 
--- Rayan_Roshan_Website/src/Pages/Projects.jsx
+++ Rayan_Roshan_Website/src/Pages/Projects.jsx
@@ -19,7 +19,8 @@ function Projects() {
     </div>
     <h2 className='Projects-title'>Projects</h2>
     <div>
-      <Project name = "Property Recommendation Software"/>
+      <Project name = "Property Recommendation Software" 
+              description = "This project is a Python-based property recommendation tool that uses feature similarity to identify the top k comparable properties to a given subject property. It reads property data from a JSON file and outputs the most similar properties using a distance-based algorithm."/>
     </div>
     </motion.div>
   );
