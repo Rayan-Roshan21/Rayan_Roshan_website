25fed2d66704ee32681ddfb01abe3a8029e04bea
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-26 21:39:04 -0400

    Added an image to the component.

--- Rayan_Roshan_Website/src/Components/Projects_component/Project.jsx
+++ Rayan_Roshan_Website/src/Components/Projects_component/Project.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import '../Projects_component/Project.css'; // Assuming you have a CSS file for styling
+import PropertyImage from '../../assets/property-image.jpg'; // Importing an image for the project
+import '../Projects_component/Project.css';
 function Project(props) {
     return (
         <>
         <div className='project-container'>
-            <p className='project-image'>{props.image}</p>
+            <img className = "project-image" src={PropertyImage} alt="Images of properties" />
             <p className='project_name'>{props.name}</p>
             <p className='project-description'>{props.description}</p>
-            <p>{props.technologies}</p>
-            <p>{props.link}</p>
             <p>{props.github}</p>
-            <p>{props.year}</p>
-            <p>{props.status}</p>
         </div>
        </>
     );
