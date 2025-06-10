c73c66dc0cc841cc423fd6fafed69d48e77b618d
Rayan Roshan <2006rayanroshan@gmail.com>
2025-06-10 00:29:59 -0400

    Made further adjustments

--- Rayan_Roshan_Website/src/Components/Projects_component/Project.jsx
+++ Rayan_Roshan_Website/src/Components/Projects_component/Project.jsx
@@ -10,13 +10,6 @@ function Project(props) {
                 <p className='project-description'>{props.description}</p>
                 <a className = "project-github" href={props.github} target="_blank" rel="noopener noreferrer">View on GitHub</a>
             </div>
-            <div className='project-container'>
-                <img className = "project-image" src={PropertyImage} alt="Related image of project." />
-                <p className='project_name'>{props.name}</p>
-                <p className='project-role'>{props.role}</p>
-                <p className='project-description'>{props.description}</p>
-                <a className = "project-github" href={props.github} target="_blank" rel="noopener noreferrer">View on GitHub</a>
-            </div>
         </div>
     );
 }
--- Rayan_Roshan_Website/src/Pages/Projects.jsx
+++ Rayan_Roshan_Website/src/Pages/Projects.jsx
@@ -4,7 +4,8 @@ import Name_title from '../Components/Name_title/Name_title';
 import Sidebar from '../Components/Sidebar/Sidebar.jsx';
 import Copyright_title from '../Components/Copyright_title/Copyright_title.jsx';
 import '../Pages_CSS/Projects.css';
-import Project from '../Components/Projects_component/Project.jsx'; // Assuming you have a Project component to import
+import '../Components/Projects_component/Project.css';
+import PropertyImage from '../assets/property-image.jpg';
 function Projects() {
   return (
     <motion.div
@@ -16,14 +17,21 @@ function Projects() {
     <div>
       <Sidebar />
       <Name_title />
-    </div>
-    <h2 className='Projects-title'>Projects</h2>
-    <div>
-      <Project name = "Property Recommendation Software" 
-        description = "This project is a Python-based property recommendation tool that uses feature similarity to identify the top k comparable properties to a given subject property. It reads property data from a JSON file and outputs the most similar properties using a distance-based algorithm."
-        role = "Software Developer"
-        github = "https://github.com/Rayan-Roshan21/Property-Recommendation-System"
-        />
+    </div>    <h2 className='Projects-title'>Projects</h2>    <div className='projects-wrapper'>
+      <div className='project-container'>
+        <img className = "project-image" src={PropertyImage} alt="Related image of project." />
+        <p className='project_name'>Property Recommendation Software</p>
+        <p className='project-role'>Software Developer</p>
+        <p className='project-description'>This project is a Python-based property recommendation tool that uses feature similarity to identify the top k comparable properties to a given subject property. It reads property data from a JSON file and outputs the most similar properties using a distance-based algorithm.</p>
+        <a className = "project-github" href="https://github.com/Rayan-Roshan21/Property-Recommendation-System" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+      </div>
+      <div className='project-container'>
+        <img className = "project-image" src={PropertyImage} alt="Related image of project." />
+        <p className='project_name'>Hehe</p>
+        <p className='project-role'>Software Developer</p>
+        <p className='project-description'>This project is a Python-based property recommendation tool that uses feature similarity to identify the top k comparable properties to a given subject property. It reads property data from a JSON file and outputs the most similar properties using a distance-based algorithm.</p>
+        <a className = "project-github" href="https://github.com/Rayan-Roshan21/Property-Recommendation-System" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+      </div>
     </div>
     </motion.div>
   );
